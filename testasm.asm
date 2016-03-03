ca375e10fef516926ec878c9032cf3c8b35f35b8
n_Arno <aquedesin@gmail.com>
2016-03-03 01:17:06 +0100

    small detail

--- testasm.asm
+++ testasm.asm
@@ -2,21 +2,15 @@
 
  .include "hdr.asm"
 
- .bank 0  
- .section "testasm" semifree
+ .section "testasm" superfree
  
 oneFunction:
 	php
 	phb
 
         sep     #$20
-        lda #$0
-        pha
-        plb ; change bank address to 0
-
         lda     6,s ; u8
 	ina
-
 	sta	6,s; u8	
 
 	plb
